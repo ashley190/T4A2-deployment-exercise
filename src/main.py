63e83a0ea36ba8dd3e0083c451070c5d50ac1eaa
ash190 <ashley_850819@yahoo.com>
2021-02-22 17:49:26 +1100

    setup and test database connection

--- src/main.py
+++ src/main.py
@@ -1,2 +1,39 @@
-def add(a, b):
-    return (a+b)
+from flask import Flask, jsonify, request
+import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = Flask(__name__)
+
+
+connection = psycopg2.connect(
+    database=os.getenv('database'),
+    user=os.getenv('user'),
+    password=os.getenv('password'),
+    host=os.getenv('host')
+)
+
+cursor = connection.cursor()
+cursor.execute("create table if not exists profile (id serial PRIMARY KEY, name varchar);")     # noqa:E501
+connection.commit()
+
+
+@app.route("/profile", methods=["GET"])
+def profile_page():
+    sql = "SELECT * FROM profile;"
+    cursor.execute(sql)
+    profile = cursor.fetchall()
+    return jsonify(profile)
+
+
+@app.route("/profile", methods=["POST"])
+def create_profile():
+    sql = "INSERT INTO profile (name) VALUES (%s);"
+    cursor.execute(sql, (request.json["name"],))
+    connection.commit()
+
+    sql = "SELECT * FROM profile ORDER BY ID DESC LIMIT 1;"
+    cursor.execute(sql)
+    profile = cursor.fetchone()
+    return jsonify(profile)
--- src/tests/test.py
+++ src/tests/test.py
@@ -1,7 +1,7 @@
-import unittest
-from main import add
+# import unittest
+# from main import add
 
 
-class TestMain(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+# class TestMain(unittest.TestCase):
+#     def test_add(self):
+#         self.assertEqual(add(2, 3), 5)
